58b5165caf00e35020648b15d68c4aef184d9681
xiayuxuan <xiayuxuan@easyretailpro.com>
2021-08-02 22:22:06 +0800

    init

---
+++ src/pages/Newtab/Newtab.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Newtab.css';
+import './Newtab.scss';
+import { Button } from 'antd';
+
+const Newtab = () => {
+  return (
+    <div className="App">
+      <Button>搜索</Button>
+    </div>
+  );
+};
+
+export default Newtab;
